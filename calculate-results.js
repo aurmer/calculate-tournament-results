96180d9c66458a233c70ab000fabbc63f10c621d
Aubrey Snider <aubrey.snider@umbrage.com>
2021-06-23 08:18:44 -0500

    pass 4

--- calculate-results.js
+++ calculate-results.js
@@ -1,6 +1,5 @@
 function calculateResultsArray (tournament) {
-  const finishedGames = Object.values(tournament.games).filter( (game) => game.status === "final" )
-  const teamsObject = finishedGames.reduce(gamesToTeamsObject.bind(null,tournament.teams),{})
+  const teamsObject = tournamentToTeamsObj(tournament)
   const teamsArray = Object.keys(teamsObject).map(teamsObjectToArray.bind(null,teamsObject))
   const rankedTeamsArray = teamsArray.sort( (a,b) => b['victory-points'] - a['victory-points'] )
                                      .map( (team,idx) => ({ ...team, place: idx+1}))
@@ -9,8 +8,7 @@ function calculateResultsArray (tournament) {
 }
 
 function calculateResultsObject (tournament) {
-  const finishedGames = Object.values(tournament.games).filter( (game) => game.status === "final" )
-  const teamsObject = finishedGames.reduce(gamesToTeamsObject.bind(null,tournament.teams),{})
+  const teamsObject = tournamentToTeamsObj(tournament)
   const rankedTeamIDs = Object.keys(teamsObject).sort( (a,b) => teamsObject[b].victoryPoints - teamsObject[a].victoryPoints )
   for (idx in rankedTeamIDs) {
     teamsObject[rankedTeamIDs[idx]].place = parseInt(idx) + 1
@@ -19,48 +17,35 @@ function calculateResultsObject (tournament) {
   return teamsObject
 }
 
-function initTeamData (team) {
-  return {
-    gamesLost: 0,
-    gamesPlayed: 0,
-    gamesTied: 0,
-    gamesWon: 0,
-    pointDiff: 0,
-    pointsLost: 0,
-    pointsPlayed: 0,
-    pointsWon: 0,
-    victoryPoints: 0,
-    teamCaptain: team.captain,
-    teamName: team.name
-  }
+function tournamentToTeamsObj (tournament) {
+  const finishedGames = Object.values(tournament.games).filter( (game) => game.status === "final" )
+  return finishedGames.reduce(gamesToTeamsObject.bind(null,tournament.teams),{})
 }
 
 function gamesToTeamsObject (teams, acc, game) {
   const gameStats = calcGameStats(game)
 
-  for(teamLetter of "AB") {
-    const TEAM_ID = game[`team${teamLetter}-id`]
-    if(!acc[TEAM_ID]) acc[TEAM_ID] = initTeamData(teams[TEAM_ID])
-  }
+  if(!acc[game[`teamA-id`]]) acc[game[`teamA-id`]] = initTeamData(teams[game[`teamA-id`]])
+  if(!acc[game[`teamB-id`]]) acc[game[`teamB-id`]] = initTeamData(teams[game[`teamB-id`]])
     
-  for(teamLetter of "AB") {
-    const TEAM_ID = game[`team${teamLetter}-id`]
-    const DID_TEAM_WIN = gameStats[`${teamLetter}Won`]
-    const DID_TEAM_LOSE = !DID_TEAM_WIN && !gameStats.isTie
-    const POINTS_WON = game[`score${teamLetter}`]
-    const POINT_DIFF = gameStats[`${teamLetter}PointsDiff`]
-    const POINTS_LOST = gameStats[`${teamLetter}PointsLost`]
-    const VIC_POINTS = gameStats[`${teamLetter}VicPoints`]
+  for(letter of "AB") {
+    const teamId = game[`team${letter}-id`]
+    const didTeamWin = gameStats[`${letter}Won`]
+    const didTeamLose = !didTeamWin && !gameStats.isTie
+    const ptWon = game[`score${letter}`]
+    const ptDiff = gameStats[`${letter}PointsDiff`]
+    const ptLost = gameStats[`${letter}PointsLost`]
+    const vicPt = gameStats[`${letter}VicPoints`]
 
-    acc[TEAM_ID].gamesLost += DID_TEAM_LOSE ? 1 : 0
-    acc[TEAM_ID].gamesPlayed += 1
-    acc[TEAM_ID].gamesTied += gameStats.isTie ? 1 : 0
-    acc[TEAM_ID].gamesWon += DID_TEAM_WIN ? 1 : 0
-    acc[TEAM_ID].pointDiff += POINT_DIFF
-    acc[TEAM_ID].pointsLost += POINTS_LOST
-    acc[TEAM_ID].pointsPlayed += gameStats.pointsPlayed
-    acc[TEAM_ID].pointsWon += POINTS_WON
-    acc[TEAM_ID].victoryPoints += VIC_POINTS
+    acc[teamId].gamesLost += didTeamLose ? 1 : 0
+    acc[teamId].gamesPlayed += 1
+    acc[teamId].gamesTied += gameStats.isTie ? 1 : 0
+    acc[teamId].gamesWon += didTeamWin ? 1 : 0
+    acc[teamId].pointDiff += ptDiff
+    acc[teamId].pointsLost += ptLost
+    acc[teamId].pointsPlayed += gameStats.pointsPlayed
+    acc[teamId].pointsWon += ptWon
+    acc[teamId].victoryPoints += vicPt
   }
 
   return acc
@@ -78,6 +63,22 @@ function calcGameStats (game) {
     BPointsDiff: game.scoreB - game.scoreA,
     AVicPoints: (game.scoreA > game.scoreB) * 3000 + (game.scoreA === game.scoreB) * 1000 + game.scoreA - game.scoreB,
     BVicPoints: (game.scoreA < game.scoreB) * 3000 + (game.scoreA === game.scoreB) * 1000 + game.scoreB - game.scoreA
+  }
+}
+
+function initTeamData (team) {
+  return {
+    gamesLost: 0,
+    gamesPlayed: 0,
+    gamesTied: 0,
+    gamesWon: 0,
+    pointDiff: 0,
+    pointsLost: 0,
+    pointsPlayed: 0,
+    pointsWon: 0,
+    victoryPoints: 0,
+    teamCaptain: team.captain,
+    teamName: team.name
   }
 }
 
