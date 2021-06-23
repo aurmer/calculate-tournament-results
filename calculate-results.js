ee40be9d08e2c3911011360b725c5251c89beb25
Aubrey Snider <aubrey.snider@umbrage.com>
2021-06-22 23:46:29 -0500

    pass 3

--- calculate-results.js
+++ calculate-results.js
@@ -1,39 +1,84 @@
 function calculateResultsArray (tournament) {
-  //filter out unfinished games from the list
-  const finishedGames = Object.keys(tournament.games).filter( (ele) => tournament.games[ele].status === "final" )
-
-  //iterate through all games to accumulate stats about teams
-  let teamsObject = finishedGames.reduce(gamesToTeamsObject.bind(null,tournament),{})
-
-  //map teamsObject to teamsArray
-  let teamsArray = Object.keys(teamsObject).map(teamsObjectToArray.bind(null,teamsObject))
-
-  //sort by victory points and assign place properties based on order
+  const finishedGames = Object.values(tournament.games).filter( (game) => game.status === "final" )
+  const teamsObject = finishedGames.reduce(gamesToTeamsObject.bind(null,tournament.teams),{})
+  const teamsArray = Object.keys(teamsObject).map(teamsObjectToArray.bind(null,teamsObject))
   const rankedTeamsArray = teamsArray.sort( (a,b) => b['victory-points'] - a['victory-points'] )
-
-  for (idx in rankedTeamsArray) {
-    rankedTeamsArray[idx].place = parseInt(idx) + 1
-  }
+                                     .map( (team,idx) => ({ ...team, place: idx+1}))
 
   return rankedTeamsArray
 }
 
 function calculateResultsObject (tournament) {
-  //filter out unfinished games from the list
-  const finishedGames = Object.keys(tournament.games).filter( (ele) => tournament.games[ele].status === "final" )
-
-  //iterate through all games to accumulate stats about teams
-  let teamsObject = finishedGames.reduce(gamesToTeamsObject.bind(null,tournament),{})
-
-  //get a copy of teams ordered by victory points
+  const finishedGames = Object.values(tournament.games).filter( (game) => game.status === "final" )
+  const teamsObject = finishedGames.reduce(gamesToTeamsObject.bind(null,tournament.teams),{})
   const rankedTeamIDs = Object.keys(teamsObject).sort( (a,b) => teamsObject[b].victoryPoints - teamsObject[a].victoryPoints )
-
-  //once sorted by victory points, assign each team their ranking
   for (idx in rankedTeamIDs) {
     teamsObject[rankedTeamIDs[idx]].place = parseInt(idx) + 1
   }
 
   return teamsObject
+}
+
+function initTeamData (team) {
+  return {
+    gamesLost: 0,
+    gamesPlayed: 0,
+    gamesTied: 0,
+    gamesWon: 0,
+    pointDiff: 0,
+    pointsLost: 0,
+    pointsPlayed: 0,
+    pointsWon: 0,
+    victoryPoints: 0,
+    teamCaptain: team.captain,
+    teamName: team.name
+  }
+}
+
+function gamesToTeamsObject (teams, acc, game) {
+  const gameStats = calcGameStats(game)
+
+  for(teamLetter of "AB") {
+    const TEAM_ID = game[`team${teamLetter}-id`]
+    if(!acc[TEAM_ID]) acc[TEAM_ID] = initTeamData(teams[TEAM_ID])
+  }
+    
+  for(teamLetter of "AB") {
+    const TEAM_ID = game[`team${teamLetter}-id`]
+    const DID_TEAM_WIN = gameStats[`${teamLetter}Won`]
+    const DID_TEAM_LOSE = !DID_TEAM_WIN && !gameStats.isTie
+    const POINTS_WON = game[`score${teamLetter}`]
+    const POINT_DIFF = gameStats[`${teamLetter}PointsDiff`]
+    const POINTS_LOST = gameStats[`${teamLetter}PointsLost`]
+    const VIC_POINTS = gameStats[`${teamLetter}VicPoints`]
+
+    acc[TEAM_ID].gamesLost += DID_TEAM_LOSE ? 1 : 0
+    acc[TEAM_ID].gamesPlayed += 1
+    acc[TEAM_ID].gamesTied += gameStats.isTie ? 1 : 0
+    acc[TEAM_ID].gamesWon += DID_TEAM_WIN ? 1 : 0
+    acc[TEAM_ID].pointDiff += POINT_DIFF
+    acc[TEAM_ID].pointsLost += POINTS_LOST
+    acc[TEAM_ID].pointsPlayed += gameStats.pointsPlayed
+    acc[TEAM_ID].pointsWon += POINTS_WON
+    acc[TEAM_ID].victoryPoints += VIC_POINTS
+  }
+
+  return acc
+}
+
+function calcGameStats (game) {
+  return {
+    AWon: game.scoreA > game.scoreB,
+    BWon: game.scoreA < game.scoreB,
+    isTie: game.scoreA === game.scoreB,
+    pointsPlayed: game.scoreA + game.scoreB,
+    APointsLost: game.scoreB,
+    BPointsLost: game.scoreA,
+    APointsDiff: game.scoreA - game.scoreB,
+    BPointsDiff: game.scoreB - game.scoreA,
+    AVicPoints: (game.scoreA > game.scoreB) * 3000 + (game.scoreA === game.scoreB) * 1000 + game.scoreA - game.scoreB,
+    BVicPoints: (game.scoreA < game.scoreB) * 3000 + (game.scoreA === game.scoreB) * 1000 + game.scoreB - game.scoreA
+  }
 }
 
 function teamsObjectToArray (teamsObject, thisTeam) {
@@ -51,70 +96,6 @@ function teamsObjectToArray (teamsObject, thisTeam) {
      "team-name": teamsObject[thisTeam].teamName,
      "victory-points": teamsObject[thisTeam].victoryPoints
    }
-}
-
-function gamesToTeamsObject (tournament, accumulator, gameId) {
-  const game = tournament.games[gameId]
-  const gameStats = calcGameStats(game)
-
-
-  let result = {
-    [game[`teamA-id`]]: {},
-    [game[`teamB-id`]]: {},
-    ...accumulator //will replace empty team objects if applicable
-  }
-
-  for(teamLetter of "AB") {
-    const TEAM_ID = game[`team${teamLetter}-id`]
-    const TEAM_WON = gameStats[`is${teamLetter}Winner`]
-    const TEAM_LOST = !TEAM_WON && !gameStats.isGameTied
-    const POINT_DIFF = gameStats[`points${teamLetter}Diff`]
-    const POINTS_LOST = gameStats[`points${teamLetter}Lost`]
-    const POINTS_WON = game[`score${teamLetter}`]
-    const VIC_POINTS = gameStats[`vicPoints${teamLetter}`]
-
-    result[TEAM_ID] = {
-      gamesLost: incrementPropertyIfTrue(TEAM_LOST,result[TEAM_ID].gamesLost),
-      gamesPlayed: incrementProperty(result[TEAM_ID].gamesPlayed),
-      gamesTied: incrementPropertyIfTrue(gameStats.isGameTied,result[TEAM_ID].gamesTied),
-      gamesWon: incrementPropertyIfTrue(TEAM_WON,result[TEAM_ID].gamesWon),
-      pointDiff: addToProperty(POINT_DIFF,result[TEAM_ID].pointDiff),
-      pointsLost: addToProperty(POINTS_LOST,result[TEAM_ID].pointsLost),
-      pointsPlayed: addToProperty(gameStats.pointsPlayed,result[TEAM_ID].pointsPlayed),
-      pointsWon: addToProperty(POINTS_WON,result[TEAM_ID].pointsWon),
-      teamCaptain: tournament.teams[TEAM_ID].captain,
-      teamName: tournament.teams[TEAM_ID].name,
-      victoryPoints: addToProperty(VIC_POINTS,result[TEAM_ID].victoryPoints)
-    }
-  }
-  return result
-}
-
-function calcGameStats (game) {
-  return {
-    isAWinner: game.scoreA > game.scoreB,
-    isBWinner: game.scoreA < game.scoreB,
-    isGameTied: game.scoreA === game.scoreB,
-    pointsPlayed: game.scoreA + game.scoreB,
-    pointsALost: game.scoreB,
-    pointsBLost: game.scoreA,
-    pointsADiff: game.scoreA - game.scoreB,
-    pointsBDiff: game.scoreB - game.scoreA,
-    vicPointsA: (game.scoreA > game.scoreB) * 3000 + (game.scoreA === game.scoreB) * 1000 + game.scoreA - game.scoreB,
-    vicPointsB: (game.scoreA < game.scoreB) * 3000 + (game.scoreA === game.scoreB) * 1000 + game.scoreB - game.scoreA
-  }
-}
-
-function incrementProperty (number = 0) {
-  return number + 1
-}
-
-function incrementPropertyIfTrue (bool1,number = 0) {
-  return (bool1) ? number + 1 : number
-}
-
-function addToProperty (sumOperand,number = 0) {
-  return number + sumOperand
 }
 
 // this line is needed to export your functions so they can be used by the test suite
